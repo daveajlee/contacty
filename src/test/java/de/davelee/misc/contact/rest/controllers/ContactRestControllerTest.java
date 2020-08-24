0a7b85fb56d0949b340ace37494acf2efeae3137
Dave Lee <dave@davelee.de>
2020-08-24 20:29:32 +0200

    Remove unused import

--- src/test/java/de/davelee/misc/contact/rest/controllers/ContactRestControllerTest.java
+++ src/test/java/de/davelee/misc/contact/rest/controllers/ContactRestControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
