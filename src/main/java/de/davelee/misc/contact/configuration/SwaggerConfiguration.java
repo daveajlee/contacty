26c6ef65ebf51e7e448260c0c3435b99656f75f6
Dave Lee <dave@davelee.de>
2020-09-15 11:19:39 +0200

    Fix unused imports

--- src/main/java/de/davelee/misc/contact/configuration/SwaggerConfiguration.java
+++ src/main/java/de/davelee/misc/contact/configuration/SwaggerConfiguration.java
@@ -7,17 +7,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Collections;
 
 /**
  * This class configures the swagger documentation.
  */
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class SwaggerConfiguration {
 
     @Bean
@@ -26,17 +23,6 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.regex("/contact.*"))
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Contact Microservice API")
-                .description("Rest API for Sending Emails")
-                .termsOfServiceUrl("http://www.davelee.de")
-                .license("License Info")
-                .licenseUrl("http://www.davelee.de")
-                .version("1.0")
                 .build();
     }
 
--- src/test/java/de/davelee/misc/contact/rest/controllers/SwaggerConfigurationTest.java
+++ src/test/java/de/davelee/misc/contact/rest/controllers/SwaggerConfigurationTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
