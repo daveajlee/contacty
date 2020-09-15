0ab223a3b900895ba3c21be392d1de76a8639502
Dave Lee <dave@davelee.de>
2020-09-15 11:22:16 +0200

    Fix final unused imports

--- src/main/java/de/davelee/misc/contact/configuration/SwaggerConfiguration.java
+++ src/main/java/de/davelee/misc/contact/configuration/SwaggerConfiguration.java
@@ -3,10 +3,8 @@ package de.davelee.misc.contact.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
